26bae73581ca8a1f4caf9be0664eea00a3d8154a
Vic Black <Vic_Black@163.com>
2018-01-17 15:25:31 +0800

    创建EDiary项目

---
+++ EDiary/MainModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDiary
+{
+    class MainModel: INotifyPropertyChanged
+    {
+        const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EDiaryDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public MainModel()
+        {
+            DataContext = new EDiaryContentDataContext(ConnectionString);
+            Status = "连接数据库中……";
+            if (!DataContext.DatabaseExists())
+            {
+                DataContext.CreateDatabase();
+                Status="数据库创建成功！";
+            }
+            else
+            {
+                Status="数据库已经存在！";
+
+            }
+        }
+
+        #region 成员
+        public string Status { get { return _Status; } set { if (_Status == value) return; _Status = value; OnPropertyChanged(nameof(Status)); } }
+        private string _Status;
+
+        public EDiaryContentDataContext DataContext { get; }
+        #endregion
+        #region INotifyPropertyChanged接口便利实现
+        private void OnPropertyChanged(string aPropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(aPropertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+    }
+}
